d7b1e05952ab45a74a397c0fedef694ff6f5a73b
Rogelio Martin Corsino <rcorsino@gmail.com>
2017-04-12 19:51:36 -0300

    Node turoial file examples from codeman

---
+++ script4.js
@@ -0,0 +1,10 @@
+// Creating Simple Server
+
+var server = require('http'); // http is a module so I dont use ./ because this is use to local module
+
+
+server.createServer(engine).listen(1337);
+
+function engine() {
+
+}
